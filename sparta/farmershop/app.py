75352083b9c4890f4c46483da6f04863ff1929bc
bestfarmer94 <sjasjaruddus@naver.com>
2023-01-07 23:08:25 +0900

    Pre-class_Day8
    Project_디붕 Day4
    
    전체적으로 자료구조를 바꿨다.
    
    json으로 한번 데였다. 조심하자.
    큰따옴표를 적극적으로 쓰자.

--- sparta/farmershop/app.py
+++ sparta/farmershop/app.py
@@ -1,4 +1,7 @@
+import json
+
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.8vasl6v.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
@@ -17,21 +20,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/today_price')
 def today_price():
-    daily_price = list(db.daily_price.find({}, {'_id': False}))
-    print(datetime.datetime.now().strftime('%Y/%m/%d'))
-    print(daily_price)
-    if len(daily_price) != 0 and daily_price[0]['date'] == datetime.datetime.now().strftime('%Y/%m/%d'):
-        return jsonify({'daily_price': daily_price})
-    else:
+    update_date = list(db.update_date.find({}, {'_id': False}))
+
+    if len(update_date) == 0 or update_date[0]['date'] != datetime.datetime.now().strftime('%Y/%m/%d'):
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
 
         id = "sjasjaruddus@naver.com"
         pwd = "1q2w3e4r!"
 
-        driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+        # driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+        driver = webdriver.Chrome()
         driver.get("https://lostark.game.onstove.com/Market")
 
         elem = driver.find_element(By.XPATH, '//*[@id="user_id"]')
@@ -45,12 +47,18 @@ def today_price():
         elem = driver.find_element(By.XPATH, '// *[ @ id = "itemList"] / thead / tr / th[1] / a').click()
         time.sleep(0.5)
 
-        price_list = [0] * 13
-        for i in range(1, 10):
-            a = str(i)
-            xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
-            num = transform(i - 1)
-            price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
+        itemDB = []
+        for i in range(0, 9):
+            num = str(transform(i))
+            xpath = '//*[@id="tbodyItemList"]/tr[' + num + ']/td[1]/div/span[1]/img'
+            xpath2 = '//*[@id="tbodyItemList"]/tr[' + num + ']/td[2]/div/em'
+            elem1 = driver.find_element(By.XPATH, xpath)
+            elem2 = driver.find_element(By.XPATH, xpath2)
+            itemDB.append({
+                "name": elem1.get_attribute('alt'),
+                "price": round(float(elem2.text)),
+                "image": elem1.get_attribute('src')
+            })
 
         elem = driver.find_element(By.XPATH,
                                    '//*[@id="lostark-wrapper"]/div/main/div/div[1]/div[1]/div[2]/button').click()
@@ -62,11 +70,17 @@ def today_price():
         elem = driver.find_element(By.XPATH, '//*[@id="itemList"]/thead/tr/th[1]/a').click()
         time.sleep(0.5)
 
-        for i in range(1, 5):
-            a = str(i)
-            xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
-            num = transform(i + 8)
-            price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
+        for i in range(9, 13):
+            num = str(transform(i))
+            xpath = '//*[@id="tbodyItemList"]/tr[' + num + ']/td[1]/div/span[1]/img'
+            xpath2 = '//*[@id="tbodyItemList"]/tr[' + num + ']/td[2]/div/em'
+            elem1 = driver.find_element(By.XPATH, xpath)
+            elem2 = driver.find_element(By.XPATH, xpath2)
+            itemDB.append({
+                "name": elem1.get_attribute('alt'),
+                "price": round(float(elem2.text)),
+                "image": elem1.get_attribute('src')
+            })
 
         elem = driver.find_element(By.XPATH,
                                    '//*[@id="lostark-wrapper"]/div/main/div/div[1]/div[1]/div[2]/button').click()
@@ -75,37 +89,42 @@ def today_price():
         driver.close()
 
         doc = {}
-        for i in range(0, 13):
-            a = str(i)
-            doc[a] = price_list[i]
+        doc["date"] = datetime.datetime.now().strftime('%Y/%m/%d')
 
-        doc['date'] = datetime.datetime.now().strftime('%Y/%m/%d')
+        db.update_date.delete_many({})
+        db.update_date.insert_one(doc)
 
-        db.daily_price.insert_one(doc)
-        daily_price = list(db.daily_price.find({}, {'_id': False}))
+        db.itemDB.delete_many({})
+        db.itemDB.insert_many(itemDB)
 
-        return jsonify({'daily_price' : daily_price})
+    itemDB = list(db.itemDB.find({}, {"_id": False}))
+
+    print(itemDB[0]['name'])
+    print(itemDB)
+    return jsonify({"daily_price": itemDB})
 
 @app.route("/save_user", methods=["POST"])
 def save_user():
-    data_receive = request.form.getlist('data_give[]')
-    user = db.user_data.find_one({'0': data_receive[0]})
+    user_data = request.form.getlist('data_give[]')
+    print(user_data)
+    user = db.user_data.find_one({'0': user_data[0]})
     msg = '영지 정보 저장 완료'
 
+    print(user)
     if user is not None:
-        db.user_data.delete_one({'0': data_receive[0]})
+        db.user_data.delete_one({'0': user_data[0]})
         msg = '영지 정보 수정 완료'
 
     doc = {
-        '0': data_receive[0],
-        '1': data_receive[1],
-        '2': data_receive[2],
-        '3': data_receive[3],
-        '4': data_receive[4]
+        '0': user_data[0],
+        '1': user_data[1],
+        '2': user_data[2],
+        '3': user_data[3],
+        '4': user_data[4]
     }
 
     db.user_data.insert_one(doc)
-    return jsonify({'msg': msg})
+    return jsonify({"msg": msg})
 
 
 @app.route("/load_user", methods=["POST"])
@@ -114,9 +133,9 @@ def load_user():
     user_data = list(db.user_data.find({'0': data_receive}, {'_id': False}))
     print(user_data)
     if len(user_data) == 1:
-        return jsonify({'user_data': user_data})
+        return jsonify({"user_data": user_data})
     else:
-        return jsonify({'user_data': '해당 닉네임으로 저장된 영지 정보가 없습니다.'})
+        return jsonify({"user_data": "해당 닉네임으로 저장된 영지 정보가 없습니다."})
 
 
 @app.route("/crawling", methods=["GET"])
@@ -139,162 +158,42 @@ def crawling():
     time.sleep(0.5)
 
     price_list = [0] * 9
-    for i in range(1, 10):
-        a = str(i)
-        xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[4]/div/em'
-        print(xpath)
-        num = transform(i - 1)
-        price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
+    for i in range(0, 9):
+        num = str(transform(i))
+        xpath2 = '//*[@id="tbodyItemList"]/tr[' + num + ']/td[4]/div/em'
+        elem2 = driver.find_element(By.XPATH, xpath2)
+        price_list[i] = elem2.text
 
-    return jsonify({'price_list': price_list})
-    # if datetime.datetime.now().strftime('%Y/%m/%d') == price['date']:
-    #     return jsonify({'price': price})
-    # else:
-    #     options = webdriver.ChromeOptions()
-    #     options.add_argument("headless")
-    #
-    #     id = "sjasjaruddus@naver.com"
-    #     pwd = "1q2w3e4r!"
-    #
-    #     driver = webdriver.Chrome('./chromedriver.exe', options=options)
-    #     driver.get("https://lostark.game.onstove.com/Market")
-    #
-    #     time.sleep(2)
-    #     elem = driver.find_element(By.XPATH, '//*[@id="user_id"]')
-    #     elem.send_keys(id)
-    #     elem = driver.find_element(By.XPATH, '//*[@id="user_pwd"]')
-    #     elem.send_keys(pwd)
-    #     elem.send_keys(Keys.RETURN)
-    #     time.sleep(2)
-    #     elem = driver.find_element(By.XPATH, '//*[@id="lostark-wrapper"]/div/main/div/div[2]/a[2]').click()
-    #     time.sleep(2)
-    #     elem = driver.find_element(By.XPATH, '// *[ @ id = "itemList"] / thead / tr / th[1] / a').click()
-    #     time.sleep(2)
-    #
-    #     price_list = []
-    #     for i in range(0, 8) :
-    #         xpath = '//*[@id="tbodyItemList"]/tr[' + i + ']/td[4]/div/em'
-    #         price_list[i] = round(float(driver.find_element(By.XPATH, xpath).text))
-    #
-    #     return jsonify({'price_list': price_list})
-    # white_relic = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[1]/td[2]/div/em').text))
-    # white_fish = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[2]/td[2]/div/em').text))
-    # white_meat = round(
-    #     float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[3]/td[2]/div/em').text))
-    # green_meat = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[4]/td[2]/div/em').text))
-    # green_fish = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[5]/td[2]/div/em').text))
-    # green_relic = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[6]/td[2]/div/em').text))
-    # blue_relic = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[7]/td[2]/div/em').text))
-    # blue_fish = round(float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[8]/td[2]/div/em').text))
-    # blue_meat = round(
-    #     float(driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[9]/td[2]/div/em').text))
+    return jsonify({"price_list": price_list})
 
-    # print(datetime.datetime.now().strftime('%Y/%m/%d'))
-    # doc = {
-    #     'date': datetime.datetime.now().strftime('%Y/%m/%d'),
-    #     'white_relic': white_relic,
-    #     'white_fish': white_fish,
-    #     'white_meat': white_meat,
-    #     'green_meat': green_meat,
-    #     'green_fish': green_fish,
-    #     'green_relic': green_relic,
-    #     'blue_relic': blue_relic,
-    #     'blue_fish': blue_fish,
-    #     'blue_meat': blue_meat
-    # }
-    # db.lostark.insert_one(doc)
-    #
-    # all_price = list(db.lostark.find({}, {'_id': False}))
-    # price = all_price[len(all_price) - 1]
-    #
-    # return jsonify({'price': price})
 def transform(num):
     if num == 0:
-        return 0
-    if num == 1:
-        return 3
-    if num == 2:
-        return 6
-    if num == 3:
-        return 7
-    if num == 4:
-        return 4
-    if num == 5:
         return 1
-    if num == 6:
+    if num == 1:
+        return 6
+    if num == 2:
+        return 7
+    if num == 3:
         return 2
-    if num == 7:
+    if num == 4:
         return 5
-    if num == 8:
+    if num == 5:
         return 8
-    if num == 9:
-        return 11
-    if num == 10:
-        return 12
-    if num == 11:
-        return 10
-    if num == 12:
+    if num == 6:
+        return 3
+    if num == 7:
+        return 4
+    if num == 8:
         return 9
+    if num == 9:
+        return 4
+    if num == 10:
+        return 3
+    if num == 11:
+        return 1
+    if num == 12:
+        return 2
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-def daily_crawling():
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-
-    id = "sjasjaruddus@naver.com"
-    pwd = "1q2w3e4r!"
-
-    driver = webdriver.Chrome()
-    driver.get("https://lostark.game.onstove.com/Market")
-
-    elem = driver.find_element(By.XPATH, '//*[@id="user_id"]')
-    elem.send_keys(id)
-    elem = driver.find_element(By.XPATH, '//*[@id="user_pwd"]')
-    elem.send_keys(pwd)
-    elem.send_keys(Keys.RETURN)
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '//*[@id="lostark-wrapper"]/div/main/div/div[2]/a[2]').click()
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '// *[ @ id = "itemList"] / thead / tr / th[1] / a').click()
-    time.sleep(0.5)
-
-    price_list = [0] * 13
-    for i in range(1, 10):
-        a = str(i)
-        xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
-        num = transform(i - 1)
-        price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
-
-    elem = driver.find_element(By.XPATH,
-                               '//*[@id="lostark-wrapper"]/div/main/div/div[1]/div[1]/div[2]/button').click()
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '//*[@id="expand-character-list"]/ul[1]/li[3]/span/button').click()
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '//*[@id="modal-info"]/div/div/div[2]/button').click()
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '//*[@id="itemList"]/thead/tr/th[1]/a').click()
-    time.sleep(0.5)
-
-    for i in range(1, 5):
-        a = str(i)
-        xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
-        num = transform(i + 8)
-        price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
-
-    elem = driver.find_element(By.XPATH,
-                               '//*[@id="lostark-wrapper"]/div/main/div/div[1]/div[1]/div[2]/button').click()
-    time.sleep(0.5)
-    elem = driver.find_element(By.XPATH, '//*[@id="expand-character-list"]/ul[1]/li[1]/span/button').click()
-    driver.close()
-
-    doc = {}
-    for i in range(0, 13):
-        a = str(i)
-        doc[a] = price_list[i]
-
-    doc['date'] = datetime.datetime.now().strftime('%Y/%m/%d')
-
-    db.daily_price.delete_many({})
-    db.daily_price.insert_one(doc)
--- sparta/farmershop/google.py
+++ sparta/farmershop/google.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 import math
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.8vasl6v.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
@@ -15,29 +16,67 @@ options.add_argument("headless")
 id = "sjasjaruddus@naver.com"
 pwd = "1q2w3e4r!"
 
+# driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
 driver = webdriver.Chrome()
 driver.get("https://lostark.game.onstove.com/Market")
 
-time.sleep(2)
 elem = driver.find_element(By.XPATH, '//*[@id="user_id"]')
 elem.send_keys(id)
 elem = driver.find_element(By.XPATH, '//*[@id="user_pwd"]')
 elem.send_keys(pwd)
 elem.send_keys(Keys.RETURN)
-time.sleep(2)
+time.sleep(0.5)
 elem = driver.find_element(By.XPATH, '//*[@id="lostark-wrapper"]/div/main/div/div[2]/a[2]').click()
-time.sleep(2)
+time.sleep(0.5)
 elem = driver.find_element(By.XPATH, '// *[ @ id = "itemList"] / thead / tr / th[1] / a').click()
-time.sleep(2)
+time.sleep(0.5)
 
-price_list = []
+itemDB = []
 for i in range(1, 10):
-	a = str(i)
-	xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[4]/div/em'
-	print(xpath)
-	price_list.append(round(float(driver.find_element(By.XPATH, xpath).text)))
+    a = str(i)
+    xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[1]/div/span[1]/img'
+    xpath2 = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
+    elem1 = driver.find_element(By.XPATH, xpath)
+    elem2 = driver.find_element(By.XPATH, xpath2)
+    itemDB.append({
+        'name': elem1.get_attribute('alt'),
+        'price': elem2.text,
+        'image': elem1.get_attribute('src')
+    })
 
-print(price_list)
+print(itemDB)
+# elem = driver.find_element(By.XPATH,
+#                            '//*[@id="lostark-wrapper"]/div/main/div/div[1]/div[1]/div[2]/button').click()
+# time.sleep(0.5)
+# elem = driver.find_element(By.XPATH, '//*[@id="expand-character-list"]/ul[1]/li[3]/span/button').click()
+# time.sleep(0.5)
+# elem = driver.find_element(By.XPATH, '//*[@id="modal-info"]/div/div/div[2]/button').click()
+# time.sleep(0.5)
+# elem = driver.find_element(By.XPATH, '//*[@id="itemList"]/thead/tr/th[1]/a').click()
+# time.sleep(0.5)
+#
+# for i in range(1, 5):
+#     a = str(i)
+#     xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[2]/div/em'
+#     price_list[num] = round(float(driver.find_element(By.XPATH, xpath).text))
+
+# elem = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[1]/td[1]/div/span[1]/img').get_attribute('src')
+# elem2 = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[1]/td[1]/div/span[1]/img').get_attribute('alt')
+# elem3 = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]/tr[1]/td[4]/div/em').text
+# itemDB = []
+# itemDB.append({
+#     'name': elem2,
+#     'price': elem3,
+#     'image': elem
+# })
+# print(itemDB)
+# price_list = []
+# for i in range(1, 10):
+# 	a = str(i)
+# 	xpath = '//*[@id="tbodyItemList"]/tr[' + a + ']/td[4]/div/em'
+# 	print(xpath)
+# 	price_list.append(round(float(driver.find_element(By.XPATH, xpath).text)))
+
 print('success')
 # print(elem)
 # assert "Python" in driver.title
